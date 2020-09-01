ae1d48737b9c6e80bbc612047bc94c05d2deda68
Gxstar <gengxing123@qq.com>
2020-09-01 23:41:30 +0800

    新的开始

---
+++ gxsite/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+
+# Create your views here.
+def show_home(request):
+    return render(request,'index.html',get_background())
+
+def get_background():
+    '''获得每日bing图片作为首页背景
+    输入：无
+    返回：dict(图片地址)
+    '''
+    context = {}
+    origin_url = "https://cn.bing.com/HPImageArchive.aspx?idx=0&n=1"
+    xml_file = requests.get(origin_url).content
+    bs_parse = BeautifulSoup(xml_file, "lxml")
+    result_url = "https://cn.bing.com"+bs_parse.url.string
+    context['url'] = result_url
+    return context

0c4c0714d7d931f84409928a0d3d33894adee643
Gxstar <gengxing123@qq.com>
2020-09-02 23:57:46 +0800

    完善首页文章列表

--- blog/views.py
+++ blog/views.py
@@ -19,10 +19,12 @@ def blog_home(request):
             mobile = True
             break
     article_list = get_article(1, 0)
+    page_list = get_page(0, 1)
     context = {
         "page": 1,
         "catid": 0,
-        "article_list": article_list
+        "article_list": article_list,
+        "page_list": page_list
     }
     if mobile is False:
         return render(request, "blog/article_list.html", context)
@@ -51,3 +53,36 @@ def get_article(page, catid):
         temp['category'] = i.category.name
         context.append(temp)
     return context
+
+
+def get_page(catid, active_page):
+    '''
+    获取总共页数
+    '''
+    if catid == 0:
+        page_count = int(Article.objects.count()/4)+1
+    else:
+        page_count = int(Article.objects.filter(
+            category__id=catid).count()/4)+1
+    pages = {}
+    pages["data"] = []
+    num_active_page = int(active_page)
+    if page_count <= 5:
+        for i in range(page_count):
+            pages["data"].insert(i, i+1)
+    else:
+        if num_active_page >= page_count-4:
+            for i in range(5):
+                pages["data"].insert(
+                    i, page_count-4+i)
+        elif 2 < num_active_page < page_count-4:
+            for i in range(5):
+                pages["data"].insert(
+                    i, num_active_page-2+i)
+        else:
+            for i in range(5):
+                pages["data"].insert(
+                    i, i+1)
+    pages["total"] = page_count
+    pages["active"] = num_active_page
+    return pages

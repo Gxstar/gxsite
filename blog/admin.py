4e9635db30bfb8c3204569c95086ab3f8c703f4a
Gxstar <gengxing123@qq.com>
2020-09-08 10:45:23 +0800

    注册博客模型

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
+# 后台模型注册页面
 from django.contrib import admin
-
+from .models import Category, Article
 # Register your models here.
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    """
+    文章模型管理
+    """
+    list_display = ('title', 'createTime', 'updateTime')
+
+
+admin.site.register(Category)
+admin.site.register(Article, ArticleAdmin)

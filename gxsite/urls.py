c7084478e588f765a6cc1adbea42960947b9fc2d
Gxstar <gengxing123@qq.com>
2020-09-24 23:04:25 +0800

    添加百度网站统计

--- gxsite/urls.py
+++ gxsite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include,re_path
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
@@ -26,7 +26,8 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('mdeditor/', include('mdeditor.urls')),
     path('captcha/', include('captcha.urls')),
-    re_path(r'^uploads/(?P<path>.*)$', serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r'^uploads/(?P<path>.*)$', serve,
+            {"document_root": settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:

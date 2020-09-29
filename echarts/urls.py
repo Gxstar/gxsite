69920ec66eb460fc51e1ba5a2f684380a8ba4d0b
Gxstar <gengxing123@qq.com>
2020-09-29 23:58:59 +0800

    学习pyecharts

--- echarts/urls.py
+++ echarts/urls.py
@@ -5,6 +5,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bar/', views.ChartView.as_view(), name='demo'),
-    path('index/', views.IndexView.as_view(), name='demo'),
+    path('', views.index, name='index'),
 ]
--- echarts/views.py
+++ echarts/views.py
@@ -1,67 +1,20 @@
-from django.shortcuts import render
-
-# Create your views here.
-import json
-from random import randrange
-
+from jinja2 import Environment, FileSystemLoader
+from pyecharts.globals import CurrentConfig
 from django.http import HttpResponse
-from rest_framework.views import APIView
+from pyecharts.globals import ThemeType
 
-from pyecharts.charts import Bar
+CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("./echarts/templates"))
+
 from pyecharts import options as opts
+from pyecharts.charts import Bar
 
 
-# Create your views here.
-def response_as_json(data):
-    json_str = json.dumps(data)
-    response = HttpResponse(
-        json_str,
-        content_type="application/json",
-    )
-    response["Access-Control-Allow-Origin"] = "*"
-    return response
-
-
-def json_response(data, code=200):
-    data = {
-        "code": code,
-        "msg": "success",
-        "data": data,
-    }
-    return response_as_json(data)
-
-
-def json_error(error_string="error", code=500, **kwargs):
-    data = {
-        "code": code,
-        "msg": error_string,
-        "data": {}
-    }
-    data.update(kwargs)
-    return response_as_json(data)
-
-
-JsonResponse = json_response
-JsonError = json_error
-
-
-def bar_base() -> Bar:
+def index(request):
     c = (
-        Bar()
+        Bar(init_opts=opts.InitOpts(theme=ThemeType.LIGHT))
         .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-        .add_yaxis("商家A", [randrange(0, 100) for _ in range(6)])
-        .add_yaxis("商家B", [randrange(0, 100) for _ in range(6)])
-        .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
-        .dump_options_with_quotes()
+        .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
+        .add_yaxis("商家B", [15, 25, 16, 55, 48, 8])
+        .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"),toolbox_opts=opts.ToolboxOpts(is_show=True))
     )
-    return c
-
-
-class ChartView(APIView):
-    def get(self, request, *args, **kwargs):
-        return JsonResponse(json.loads(bar_base()))
-
-
-class IndexView(APIView):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse(content=open("./templates/index.html").read())
+    return HttpResponse(c.render_embed())
